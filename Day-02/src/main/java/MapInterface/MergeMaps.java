ac601083adca938f28264bb8708e736e4680894c
kritz999 <kritikabdas007@gmail.com>
2025-02-16 12:59:48 +0530

    [Kritika B.Das] add : merge maps

---
+++ Day-02/src/main/java/MapInterface/MergeMaps.java
@@ -0,0 +1,34 @@
+package MapInterface;
+import java.util.*;
+
+    public class MergeMaps {
+        public static Map<String, Integer> mergeMaps(Map<String, Integer> map1, Map<String, Integer> map2) {
+            Map<String, Integer> mergedMap = new HashMap<>(map1); // Copy map1 into mergedMap
+
+            for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+                // Sum values if key exists, otherwise put new key
+                mergedMap.put(entry.getKey(), mergedMap.getOrDefault(entry.getKey(), 0) + entry.getValue());
+            }
+
+            return mergedMap;
+        }
+
+        public static void main(String[] args) {
+            // Example Input
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("A", 1);
+            map1.put("B", 2);
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("B", 3);
+            map2.put("C", 4);
+
+            // Merge maps
+            Map<String, Integer> mergedMap = mergeMaps(map1, map2);
+
+            // Print merged map
+            System.out.println(mergedMap);
+        }
+    }
+
+
